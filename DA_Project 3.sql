aaf68f6d327f4e3d59fe339be7bf7a90918b97fc
avisekbose <avisek.bose@hotmail.com>
2026-04-04 12:35:26 +0530

    SQL Initial Commit

---
+++ DA_Project 3.sql
@@ -0,0 +1,71 @@
+# Loading Schema
+use `decodelabs-internship`;
+
+# Viewing Dataset
+select * from `datasetfordataanalytics`;
+
+# Identifing Missing Values
+select * from datasetfordataanalytics
+where CouponCode is null or trim(CouponCode) = '';
+
+set sql_safe_updates = 0;
+
+# Imputing Missing Values
+update datasetfordataanalytics
+set CouponCode = 'No Coupon'
+where CouponCode is null or trim(CouponCode) = '';
+
+set sql_safe_updates = 1;
+
+# Checking imputation of missing values
+select * from datasetfordataanalytics
+where CouponCode = 'No Coupon';
+
+# Identifying High Value Orders
+select * from datasetfordataanalytics
+where TotalPrice > 1000;
+
+# Identifying Orders using Coupons
+select * from datasetfordataanalytics
+where CouponCode != 'No Coupon';
+
+# Total Revenue
+select round(SUM(TotalPrice),2) as TotalRevenue
+from datasetfordataanalytics;
+
+# Total Orders
+select count(*) as TotalOrders
+from datasetfordataanalytics;
+
+# Avg. Order values
+select round(avg(TotalPrice),2) as AvgOrderValue
+from datasetfordataanalytics;
+
+# Top Performing Products
+select Product, count(*) as Orders, round(sum(TotalPrice),2) as Revenue
+from datasetfordataanalytics
+group by Product
+order by Revenue desc;
+
+# Customer Payment Preference
+select 	PaymentMethod, count(*) as Transactions, round(sum(TotalPrice),2) as Revenue
+from datasetfordataanalytics
+group by PaymentMethod
+order by Revenue desc;
+
+# Coupon Performance
+select CouponCode, count(*) as CouponUsage, round(sum(TotalPrice),2) as Revenue
+from datasetfordataanalytics
+group by CouponCode
+order by CouponUsage desc;
+
+# Monthly Trend
+select date_format(Date, '%Y-%m') as YearMonth, count(*) as Orders, round(sum(TotalPrice),2) as Revenue
+from datasetfordataanalytics
+group by YearMonth
+order by YearMonth;
+
+# Identifying High Value / Premium Customers
+select * from datasetfordataanalytics
+order by TotalPrice desc
+limit 10;
